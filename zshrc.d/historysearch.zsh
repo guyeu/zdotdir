43ff7f96338ed44d05933fa047e39de1128ea076
hujia <hujia@topjoy.com>
2023-03-13 11:57:11 +0800

    enable fzf

--- zshrc.d/historysearch.zsh
+++ zshrc.d/historysearch.zsh
@@ -1,2 +1,11 @@
+# enable fuzzy searching, higher numbers weight toward shorter matches.
+export MCFLY_FUZZY=2
+
+# move interface view to bottom.
+export MCFLY_INTERFACE_VIEW=BOTTOM
+
+# to swap the color scheme for use in a light terminal.
+export MCFLY_LIGHT=TRUE
+
 eval "$(mcfly init zsh)"
 
