85e3ff5225583e42b9bc4a096f59ef16d3fe0be7
hujia <hujia@topjoy.com>
2023-03-15 15:53:00 +0800

    import fzf-tab

---
+++ zshrc.d/fzf.zsh
@@ -0,0 +1,9 @@
+# 
+# fzf-zsh
+#
+
+# see https://github.com/Aloxaf/fzf-tab/wiki/Configuration#default-color
+zstyle ':fzf-tab:*' default-color $'\033[30m'
+# # see https://man.archlinux.org/man/fzf.1.en#color=
+zstyle ':fzf-tab:*' fzf-flags --color=light
+
